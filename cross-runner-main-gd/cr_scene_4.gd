84151250d0e4bb8d8ddc7119319d356bfe6967a6
Gjilde <gildeoliver07@gmail.com>
2025-11-28 09:30:19 +0100

    sum advanced knowleg stuff

--- cross-runner-main-gd/cr_scene_4.gd
+++ cross-runner-main-gd/cr_scene_4.gd
@@ -1,11 +1,33 @@
-extends Node2D
+extends Node
 
+@export var main_menu_scene: String = "res://MainMenu.tscn"
+var is_paused := false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# På ready kobler vi knappene og skjuler popup
+func _ready():
+	# Popup starter skjult
+	$CanvasLayer/PausePopup.visible = false
 
+	# Koble knappene til funksjoner
+	var resume_button = $CanvasLayer/PausePopup/VBoxContainer/resume
+	var mainmenu_button = $CanvasLayer/PausePopup/VBoxContainer/mainmenu
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	resume_button.pressed.connect(_on_resume_pressed)
+	mainmenu_button.pressed.connect(_on_main_menu_pressed)
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"): # ESC
+		toggle_pause()
+
+func toggle_pause():
+	is_paused = !is_paused
+	get_tree().paused = is_paused
+	$CanvasLayer/PausePopup.visible = is_paused
+
+func _on_resume_pressed():
+	toggle_pause()
+
+func _on_main_menu_pressed():
+	# Gå til Main Menu og fjern pause
+	get_tree().paused = false
+	get_tree().change_scene_to_file(main_menu_scene)
